ddb43136e3dcecad8f2a3be2f53bc2fbc8e56a6c
xGabrielx8 <gabrielmbabinski@gmail.com>
2024-04-29 10:32:06 -0400

    a

--- app/src/main/java/com/example/trabalho_ademar_app/MainActivity.java
+++ app/src/main/java/com/example/trabalho_ademar_app/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.Switch;
@@ -14,7 +15,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     //checkbox//
-    private CheckBox checkOp1, checkOp2,checkOp3;
+    private CheckBox checkOp1, checkOp2, checkOp4;
 
     //radio button//
     private RadioButton radio1, radio2;
@@ -28,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
     //button//
     private Button cadastrar;
 
+    private ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,24 +40,31 @@ public class MainActivity extends AppCompatActivity {
         //checkbox//
         checkOp1=findViewById(R.id.checkBox1);
         checkOp2=findViewById(R.id.checkBox2);
-        checkOp3=findViewById(R.id.checkBox3);
+        checkOp4=findViewById(R.id.checkBox4);
 
         //radiobutton//
-        radio1 = findViewById(R.id.radioButton);
+        radio1 = findViewById(R.id.radiobutton1);
 
         //spinner//
-        camiseta =findViewById(R.id.spinner);
+        camiseta =findViewById(R.id.camiseta);
 
         //switch//
         notificacao= findViewById(R.id.switch1);
 
         cadastrar = findViewById(R.id.button);
 
+        //progress bar//
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.INVISIBLE);
+
         cadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //shared preferences//
             SalvarArquivoSharedPreferences();
+
+            //progressBar//
+                progressBar.setVisibility(View.VISIBLE);
             }
 
             private void SalvarArquivoSharedPreferences() {
@@ -63,6 +73,17 @@ public class MainActivity extends AppCompatActivity {
                 //checkbox//
                 editor.putBoolean("box1", checkOp1.isChecked());
                 editor.putBoolean("box2", checkOp2.isChecked());
+
+                //radiobutton//
+                editor.putBoolean("escolha1",radio1.isChecked());
+                editor.putBoolean("escolha2",radio2.isChecked());
+                //spinner//
+                editor.putString("camiseta",camiseta.getSelectedItem().toString());
+                //switch//
+                editor.putBoolean("notificação",notificacao.isChecked());
+
+                //salvar shared preferences//
+                editor.apply();
             }
         });
 
